6aaf08b01e95f4a322c3096cab3d610d80967dd7
seunghyunnn <rofam2@naver.com>
2025-06-01 20:33:24 +1000

    Add winner calculation logic in main function

--- src/main/PokerHandSorter.java
+++ src/main/PokerHandSorter.java
@@ -10,6 +10,8 @@ public class PokerHandSorter {
     public static void main(String[] args) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String line;
+        int player1WinsCount = 0;
+        int player2WinsCount = 0;
         try {
             while ((line=reader.readLine()) != null) {
                 List<Card> cards = new ArrayList<>();
@@ -19,11 +21,16 @@ public class PokerHandSorter {
                 }
                 Hand hand1 = new Hand(cards.subList(0, 5));
                 Hand hand2 = new Hand(cards.subList(5, 10));
-                int rank1 = hand1.getRank().getValue();
-                int rank2 = hand2.getRank().getValue();
+                int result = hand1.compareTo(hand2);
+                if (result > 0) player1WinsCount++;
+                else if (result < 0) player2WinsCount++;
+                // If both hands have an identical set, neither wins.
             }
         } catch(IOException e) {
-            System.out.println("An error occurred while reading the file.");
+            System.out.println("An error occurred while reading the file. Finishing the program...");
+        } finally {
+            System.out.println("Player 1: " + player1WinsCount + " hands.");
+            System.out.println("Player 2: " + player2WinsCount + " hands.");
         }
     }
 }
